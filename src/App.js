cac41c6d0a93f6fb324f0e74731d43b22366ea42
Oleh Sliusar <olehsliusar@gmail.com>
2021-09-07 21:14:41 +0300

    Add size to images, and other changes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 
 function Gallery() {
   const array = ['1', '2', '3', '4', '5', '6']
-  const images = array.map(image => {
-    return <img key={image} src={require(`./images/${image}/1.jpg`).default} className="img-responsive" alt={"wedding dress "+image}/>
+  const images = array.map(name => {
+    return <img key={name} src={require(`./images/${name}/1.jpg`).default} className="Img-responsive" alt={"wedding dress "+name}/>
   });
   console.log(images[0]);
   return (
     <div className="container">
       <div className="row">
         <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-            <h4>Our gallery</h4>
+            <h4>Наша галерея</h4>
         </div>
         <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
           { images }
