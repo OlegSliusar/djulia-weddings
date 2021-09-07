e38cb0a7e2dcb6cebe9e8bfa4ddce5988a9d4550
Oleh Sliusar <olehsliusar@gmail.com>
2021-09-07 21:19:48 +0300

    Change h4 to h2 tag

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function Gallery() {
     <div className="container">
       <div className="row">
         <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-            <h4>Наша галерея</h4>
+            <h2>Наша галерея</h2>
         </div>
         <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
           { images }
