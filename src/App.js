c471d743d06ab9308521f74a0d5bd31c94f7b876
Oleh Sliusar <17471140+OlegSliusar@users.noreply.github.com>
2021-09-14 13:56:48 +0300

    Created button with value (no logistics yet)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import './App.css';
 import React from 'react';
 
-class Header extends React.Component {    
-  constructor(props) {
-      super(props);
-  }
-
+class Header extends React.Component {
   render() {
     return (
       <header className="App-header">
@@ -15,31 +11,61 @@ class Header extends React.Component {
   }
 }
 
-function Gallery() {
-  const array = ['1', '2', '3', '4', '5', '6']
-  const images = array.map(name => {
-    return <img key={name} src={require(`./images/${name}/1.jpg`).default} className="Img-responsive" alt={"wedding dress "+name}/>
-  });
-  console.log(images[0]);
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="Gallery-header">
-            <h2>Наша галерея</h2>
-        </div>
-        <div className="Gallery">
-          { images }
+function Button(props) {
+  return(
+      <button>
+        {props.value}
+      </button>
+  );
+}
+
+class Gallery extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    // const value = "no value";
+    this.state = {
+      view: "no",
+    };
+
+    if (props.value === 1) {
+      this.state.view = 'Grid';
+    } else {
+      this.state.view = 'List';
+    }
+
+    // this.setState((state, props) => ({
+    //   value: props.value
+    // }));
+  }
+
+  render() {
+    const array = ['1', '2', '3', '4', '5', '6']
+    const images = array.map(name => {
+      return <img key={name} src={require(`./images/${name}/1.jpg`).default} className="Img-responsive" alt={"wedding dress "+name}/>
+    });
+    // console.log(images[0]);
+    return (
+      <div className="container">
+        <Button value={this.state.view}/>
+        <div className="row">
+          <div className="Gallery-header">
+              <h2>Наша галерея</h2>
+          </div>
+          <div className="Gallery">
+            { images }
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Gallery />
+      <Gallery value={1}/>
     </div>
   );
 }
