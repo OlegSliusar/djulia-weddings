d0ab5b4515206fdf728d3854e3f7a04a202bf631
Oleh Sliusar <17471140+OlegSliusar@users.noreply.github.com>
2021-09-19 16:33:10 +0300

    Slider not finished

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.css';
+import Carousel from 'react-bootstrap/Carousel';
 
+// const photosUrl = 'https://jsonplaceholder.typicode.com/photos'
 
 class Header extends React.Component {
   render() {
@@ -19,15 +23,27 @@ class Header extends React.Component {
 }
 
 class Gallery extends React.Component {
+  state = {
+    image_urls: [],
+  }
+
   render() {
+    // axios.get(photosUrl).then((response) => {
+    //   const urls = response.data.slice(0,10).map(x => x.url)
+    //   this.setState({image_urls: urls})
+    // });
+
     const array = ['1', '2', '3', '4', '5', '6']
+    // const images = this.state.image_urls.map(url => {
+    //   return <img key={url} src={url} className="Img" className="Img-gallery" alt={url}/>
+    // });
     const images = array.map(name => {
       return <img key={name} src={require(`./images/${name}/1.jpg`).default} className="Img" className="Img-gallery" alt={"wedding dress "+name}/>
     });
     return (
       <div>
         {/* <Button view={this.state.view} handleClick={this.handleClick.bind(this)}/> */}
-        <Link to="/list"><button>Список</button></Link>
+        <Link to="/"><button>Список</button></Link>
         <div className="row">
           <div className="Gallery-header">
               <h2>Наша галерея</h2>
@@ -43,17 +59,27 @@ class List extends React.Component {
   render() {
     const array = ['1', '2', '3', '4', '5', '6']
     const images = array.map(name => {
-      return <img key={name} src={require(`./images/${name}/1.jpg`).default} className="Img" className="Img-list" alt={"wedding dress "+name}/>
+      return(
+        <Carousel.Item interval={1000} key={name}>
+          <img src={require(`./images/${name}/1.jpg`).default} className="Img-list" alt={"wedding dress "+name}/>
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      );
     });
     return (
-      <div>
+      <div style={{ display: 'block', width: 700, padding: 30 }}>
         {/* <Button view={this.state.view} handleClick={this.handleClick.bind(this)}/> */}
         <Link to="/gallery"><button>Галерея</button></Link>
         <div className="row">
           <div className="Gallery-header">
               <h2>Наш список</h2>
           </div>
-          {images}
+          <Carousel>
+            {images}
+          </Carousel>
         </div>
       </div>
     );
@@ -69,7 +95,7 @@ function App() {
           <Route path="/gallery">
               <Gallery />
           </Route>
-          <Route path="/list">
+          <Route path="/">
             <List />
           </Route>
         </Switch>
